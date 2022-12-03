dad84200df2de4dad8938a231a6ac3e822a8d9b0
Ivan Prikazchikov <89651706+derus15@users.noreply.github.com>
2022-12-03 16:50:38 +0400

    wrapping the code in a function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-  let column_1 = document.querySelector('.password_1');
+let column_1 = document.querySelector('.password_1');
   let column_2 = document.querySelector('.password_2');
   let column_3 = document.querySelector('.password_3');
-  let button = document.querySelector('button');
+  let button_add = document.querySelector('button');
   let input = document.querySelector('input');
   let count = document.querySelector('.count_in');
   let attention = document.querySelector('.attention');
@@ -13,43 +13,36 @@
   let len_column = 0;
   attention.hidden = true;
 
+  function CreateElement(ul,li){
+      for (let j = 0; j < input.value; j++){
+          num = Math.floor(Math.random() * 100);
+          password_b += char[num];
+          li.textContent = password_b;
+          ul.append(li);
+      }
+}
 
-
-button.onclick = function() {
-    for (let i = 0; i < count.value; i++){
+button_add.onclick = function() {
+    for (let i = 0; i < count.value; i++) {
         let li = document.createElement('li');
         password_b = ''
-        len_column ++;
-        if (len_column <= 15){
-            for (let j = 0; j < input.value; j++) {
-                num = Math.floor(Math.random() * 100);
-                password_b += char[num];
-        }
-        li.textContent = password_b;
-        column_1.append(li);
+        len_column++;
+
+        if (len_column <= 15) {
+            CreateElement(column_1, li);
         }
 
-        if(len_column > 15 && len_column <= 30){
-            for (let j = 0; j < input.value; j++) {
-                num = Math.floor(Math.random() * 100);
-                password_b += char[num];
-        }
-        li.textContent = password_b;
-        column_2.append(li);
+        if (len_column > 15 && len_column <= 30) {
+            CreateElement(column_2, li);
         }
 
-        if(len_column > 30){
-            for (let k = 0; k < input.value; k++) {
-                num = Math.floor(Math.random() * 100);
-                password_b += char[num];
+        if (len_column > 30) {
+            CreateElement(column_3, li);
         }
-        li.textContent = password_b;
-        column_3.append(li);
-        }
-        console.log(len_column)
-        if(len_column === 45){
+
+        if (len_column === 45) {
             attention.hidden = false;
-            button.setAttribute("disabled", "disabled");
+            button_add.setAttribute("disabled", "disabled");
             break
         }
     }
@@ -58,9 +51,9 @@ button.onclick = function() {
 button_delete.onclick = function() {
     let list = document.querySelectorAll('ul');
     for (let column of list){                            
-        column.innerHTML = ''
+        column.textContent = ''
     }
     attention.hidden = true;
-    button.removeAttribute("disabled");
+    button_add.removeAttribute("disabled");
     len_column = 0;
 }
